23e66f9395323e31b9a2747795c9497a7b682641
v a r r <91899516+VarrelKun@users.noreply.github.com>
2025-03-19 03:31:27 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+const PORT = 3000;
+
+app.set("json spaces", 2);
+app.use(express.json());
+
+app.use('/api', routes);
+
+// Menampilkan daftar route dalam "/api"
+app.get('/', (req, res) => {
+    res.json({
+        message: "Welcome to Drakor API",
+        endpoints: {
+            home: "/api/home",
+            detail: "/api/detail/:slug",
+            episode: "/api/episode/:slug",
+            search: "/api/search/:search"
+        }
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Online http://localhost:${PORT}`);
+});
