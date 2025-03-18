82b95369644ba121ba1bfd3b0a3074f7522452db
v a r r <91899516+VarrelKun@users.noreply.github.com>
2025-03-19 05:22:26 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
 
 app.set("json spaces", 2);
 app.use(express.json());
+app.use(cors())
 
 app.use('/api', routes);
 
