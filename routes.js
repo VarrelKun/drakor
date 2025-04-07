873262ea1416bb5d7b23bfed09e0e8ac6d7dd66a
v a r r <91899516+VarrelKun@users.noreply.github.com>
2025-04-07 10:11:19 +0700

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const { scrapeHome, scrapeDetail, topDrama, episodeDetail, searchDrama } = require('./scraper/drakorScraper')
+const { scrapeHome, scrapeDetail, dramaHome, episodeDetail, searchDrama, movieHome, movieDetail } = require('./scraper/drakorScraper')
 
 const router = express.Router();
 
 router.get('/home', async (req, res) => {
     const data = await scrapeHome();
+    res.json(data);
+});
+
+router.get('/drama/:page', async (req, res) => {
+	const { page } = req.params;
+    const data = await dramaHome(page);
+    res.json(data);
+});
+
+router.get('/movie/:page', async (req, res) => {
+	const { page } = req.params;
+    const data = await movieHome(page);
     res.json(data);
 });
 
@@ -16,6 +28,12 @@ router.get('/top', async (req, res) => {
 router.get('/detail/:slug', async (req, res) => {
     const { slug } = req.params;
     const data = await scrapeDetail(slug);
+    res.json(data);
+});
+
+router.get('/movie/x/:slug', async (req, res) => {
+    const { slug } = req.params;
+    const data = await movieDetail(slug);
     res.json(data);
 });
 
